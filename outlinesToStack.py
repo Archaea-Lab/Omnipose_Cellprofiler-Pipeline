36abfa05644aa633048b77374823147c1c1bca4c
Archaea-Lab <126028630+Archaea-Lab@users.noreply.github.com>
2024-01-18 12:18:44 -0500

    Update outlinesToStack.py

--- outlinesToStack.py
+++ outlinesToStack.py
@@ -2,22 +2,26 @@ from ij import IJ, ImagePlus, ImageStack
 import os
 import re
 
-loadFolder = IJ.getDirectory("Input_directory")
-files = os.listdir(loadFolder)
-tempList = []
-for f in files:
-	if f[-4:] == '.png':
-		tempList.append(f)
-files = tempList
-files.sort(key=lambda x: int(re.search(r'^(\d+)_', x).group(1)))
-stack = ImageStack()
 
-for frame in files:
-    file_path = os.path.join(loadFolder, frame)
-    imp = IJ.openImage(file_path)
-    stack.addSlice(frame, imp.getProcessor())
-    imp.close()
+def main():
+	loadFolder = IJ.getDirectory("Input_directory")
+	files = os.listdir(loadFolder)
+	tempList = []
+	for f in files:
+		if f[-4:] == '.png':
+			tempList.append(f)
+	files = tempList
+	files.sort(key=lambda x: int(re.search(r'^(\d+)_', x).group(1)))
+	stack = ImageStack()
+
+	for frame in files:
+   		file_path = os.path.join(loadFolder, frame)
+   		imp = IJ.openImage(file_path)
+   		stack.addSlice(frame, imp.getProcessor())
+   		imp.close()
     
-output_image = ImagePlus("Stacked Images", stack)
-output_image.setDimensions(1,1,len(files)) #set channels, Z, and T appropriately
-output_image.show()
+	output_image = ImagePlus("Stacked Images", stack)
+	output_image.setDimensions(1,1,len(files)) #set channels, Z, and T appropriately
+	output_image.show()
+
+main()
