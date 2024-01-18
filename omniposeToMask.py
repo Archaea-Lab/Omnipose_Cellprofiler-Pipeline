1f4f1d53af3556cd9857946a743f349a8c37c55a
Archaea-Lab <126028630+Archaea-Lab@users.noreply.github.com>
2024-01-18 12:17:09 -0500

    Update omniposeToMask.py

--- omniposeToMask.py
+++ omniposeToMask.py
@@ -3,49 +3,37 @@ from ij.plugin.frame import RoiManager
 import os
 import re
 
-loadFolder = IJ.getDirectory("Input_directory")
-files = os.listdir(loadFolder)
-#look only at .tifs
-tempList = []
-for f in files:
-	if f[-4:] == '.tif':
-		tempList.append(f)
-files = tempList
-print(files)
-#sort the files
-files.sort(key=lambda x: int(re.search(r'^(\d+)_', x).group(1)))
-print(files)
-#create empty stack
-stack = ImageStack()
-IJ.setForegroundColor(255, 255, 255)
-# Loop through each file in the directory
-print(files)
-for file_name in files:
-	#Create an ImagePlus from the file
-    file_path = os.path.join(loadFolder, file_name)
-    imp = IJ.openImage(file_path)
-	#open associated ROIs
-    rm = RoiManager().getInstance()
-    rm.reset()
-    rm.open(file_path.split('_label')[0]+"_Erosion_1px_RoiSet.zip")
-	#run through the ROIs and color white
-    roiList = rm.getRoisAsArray()
-    i = list(range(len(roiList)))
-    rm.setSelectedIndexes(i)
-    rm.runCommand(imp,"Combine")
-    rm.runCommand(imp,"Fill")
 
-	#add image to stack
-    stack.addSlice(file_name, imp.getProcessor())
-	# Close the ImagePlus to free up resources
-    #imp.close()
-    
-#save stack
-#create stack as a physical image
-output_image = ImagePlus("Stacked Images", stack)
-#set channels, Z, and T appropriately
-output_image.setDimensions(1,1,len(files))
-#Display the new stack
-output_image.show()
+def main():
+	loadFolder = IJ.getDirectory("Input_directory")
+	files = os.listdir(loadFolder)
+	tempList = []
+	for f in files:
+		if f[-4:] == '.tif':
+			tempList.append(f)
+	files = tempList
+	print(files)
+	files.sort(key=lambda x: int(re.search(r'^(\d+)_', x).group(1)))
+	print(files)
+	stack = ImageStack()
+	IJ.setForegroundColor(255, 255, 255)
+	for file_name in files:
+    		file_path = os.path.join(loadFolder, file_name)
+    		imp = IJ.openImage(file_path)
+    		rm = RoiManager().getInstance()
+    		rm.reset()
+    		rm.open(file_path.split('_label')[0]+"_Erosion_1px_RoiSet.zip")
+    		roiList = rm.getRoisAsArray()
+    		i = list(range(len(roiList)))
+    		rm.setSelectedIndexes(i)
+    		rm.runCommand(imp,"Combine")
+    		rm.runCommand(imp,"Fill")
+    		stack.addSlice(file_name, imp.getProcessor())
+		# Close the ImagePlus to free up resources
+    		#imp.close()
+	output_image = ImagePlus("Stacked Images", stack)
+	output_image.setDimensions(1,1,len(files)) #set channels, Z, and T appropriately
+	output_image.show()
 
+main()
 
