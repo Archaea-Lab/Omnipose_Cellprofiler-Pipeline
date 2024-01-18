a397796b044a0132ea816b42f7da4c30ed9d2922
Archaea-Lab <126028630+Archaea-Lab@users.noreply.github.com>
2024-01-18 13:44:24 -0500

    Update _label.py

--- _label.py
+++ _label.py
@@ -5,19 +5,25 @@ Created on Fri Dec  8 11:02:40 2023
 @author: johnmallon
 
 This program will open a folder of files and add the extension to the file name "_label"
+
+This program should be run in the imageJ macro window.
 """
 import os
+from ij import IJ
 
 
 def main():
-    folder = R"H:\Halofilin Shape Transitions\KO Shape Transitions\HalB KO\HalBDisktoRod_FullField\masks\\"
+    print("Starting...")
+    loadFolder = IJ.getDirectory("Input_directory")
     files = os.listdir(folder)
     for file in files:
-        oldPath = os.path.join(folder, file)
+        oldPath = os.path.join(loadFolder, file)
         if os.path.isfile(oldPath):
             newName = file.split('.')[0] +'_label.tif'
-            newPath = os.path.join(folder, newName)
+            newPath = os.path.join(loadFolder, newName)
             os.rename(oldPath, newPath)
+    print(" '_label' added as suffix to all files.")
+    print("Done!")
 
 main()
 
