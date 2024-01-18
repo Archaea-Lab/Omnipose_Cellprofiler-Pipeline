a55b9f5e4db693611b9f972fc4c588094894b5b4
Archaea-Lab <126028630+Archaea-Lab@users.noreply.github.com>
2024-01-18 12:20:19 -0500

    Update splitStacks.py

--- splitStacks.py
+++ splitStacks.py
@@ -3,32 +3,36 @@
 from ij import IJ, ImagePlus
 import os
 
-loadFolder = IJ.getDirectory("Input_directory")
-for file in os.listdir(loadFolder):
-	f= os.path.join(loadFolder, file)
-# Get the active image stack
 
-imp = IJ.openImage(f)
-stack = imp.getStack()
+def main():
+	loadFolder = IJ.getDirectory("Input_directory")
+	for file in os.listdir(loadFolder):
+		f= os.path.join(loadFolder, file)
+	# Get the active image stack
 
-# Get the number of slices in the stack
-n_slices = stack.getSize()
-print(n_slices)
-# Loop through each slice in the stack
-for slice_index in range(1, n_slices + 1):
-    # Select the current slice
-    imp.setSlice(slice_index)
+	imp = IJ.openImage(f)
+	stack = imp.getStack()
 
-    # Duplicate the current slice
-    duplicate = stack.getProcessor(slice_index).duplicate()
-    duplicate_image = ImagePlus("Slice_" + str(slice_index), duplicate)
+	# Get the number of slices in the stack
+	n_slices = stack.getSize()
+	print(n_slices)
+	# Loop through each slice in the stack
+	for slice_index in range(1, n_slices + 1):
+    		# Select the current slice
+    		imp.setSlice(slice_index)
 
-    # Save the duplicated slice
-    save_path = loadFolder + str(slice_index) + ".tif"
-    IJ.save(duplicate_image, save_path)
+    		# Duplicate the current slice
+    		duplicate = stack.getProcessor(slice_index).duplicate()
+   		duplicate_image = ImagePlus("Slice_" + str(slice_index), duplicate)
 
-    # Close the duplicated slice
-    duplicate_image.close()
+    		# Save the duplicated slice
+    		save_path = loadFolder + str(slice_index) + ".tif"
+    		IJ.save(duplicate_image, save_path)
 
-# Close the original stack
-imp.close()
+    		# Close the duplicated slice
+   		 duplicate_image.close()
+
+	# Close the original stack
+	imp.close()
+
+main()
