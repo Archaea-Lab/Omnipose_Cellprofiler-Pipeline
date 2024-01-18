bcf6e3e616e54a886ccc7346dc418c2bbee3a506
Archaea-Lab <126028630+Archaea-Lab@users.noreply.github.com>
2024-01-18 11:50:42 -0500

    Update CP_analysis_withTrackAnalysis.py

--- CP_analysis_withTrackAnalysis.py
+++ CP_analysis_withTrackAnalysis.py
@@ -3,12 +3,24 @@ Created on Wed Oct  4 13:03:06 2023
 
 @author: johnmallon
 
-This Program takes output information for Cell Profiler, running Cellpose, and graphs the information.
+This Program takes output information for Cell Profiler, running Cellpose/Omnipose, and prepares the data to be used for downstream analysis
+and graphing.
 
-Specifically, its graphs:
-    
-1.area of time
-2.circularity over time, for rods and disks separetly
+CellProfiler has unique identifiers for lineages but no clear distinction for daughter cells. Half the daughters are given the same 'label' as the
+mother and the other have get unique identifiers. This program uses the time of division calculated during CellProfiler's LAP tracking ('TrackObjects_LinkType')
+to split all cells up within lineages and give them a unique 'Cell ID' number. This allows for easier manipulation of data in our downstream analysis.
+
+Input:
+1. CellProfiler datatable that contains all the single cell information over time. It is by default called 'FilterObjects.csv'
+
+Output: 
+1. A new '.csv' file containing single cell data along with the unique 'Cell IDs'
+2. A new '.tif' stack that maps the LAP tracking done by CellProfiler onto of the segmentation done by Omnipose.
+3. A graph of Area vs. Time is output for all single cells tracked for you to generally asses if things are working.
+
+Caveats:
+1. This program does not deal with a value of 3 (mitosis) or 4 (gap in track) from CellProfiler's 'TrackObjects_LinkType'
+
 
 """
 import pandas as pd
@@ -19,14 +31,14 @@ from skimage import io
 from tqdm import tqdm
 
 def calculateShape(df,pixelConversion):
-    #calculate shape parameters
-    #convert pixels to um
+    """
+    CellProfiler makes its measurements with pixel as the unit. This program converts some of the data into the more usable unit of microns based on
+    your microscope's camera resolution and objective used.
+
+    This function returns a modified version of the input dataframe with the newly calculated columns
+    """
     df['AreaShape_Area_um']= df['AreaShape_Area']*(pixelConversion**2)
-    
-    #calculate circularity, this is the reciprocal of the measurement "compactness" that cell profiler makes 
-    df['Circularity']= 1/df['AreaShape_Compactness']
-    
-    #calculate Aspect Ratio, and asign rod vs. disk
+    df['Circularity']= 1/df['AreaShape_Compactness'] #circularity is the reciprocal of compactness
     df['Aspect Ratio']= df['AreaShape_MajorAxisLength']/df['AreaShape_MinorAxisLength']
     df['Shape'] = np.where(df['Aspect Ratio'] >= 2, 'rod', 'disk')
     
@@ -34,33 +46,34 @@ def calculateShape(df,pixelConversion):
 
 
 def findStartingCells(df):
+    """
+    Get the cells that start lineages and splits, and start separating the cells. These are cells with a 'TrackObjects_LinkType' of 0 and 2,
+    respectively. Only one daughter in a division even gets the 'TrackObjects_LinkType' of 2 so to get the other daughter cell we look for the
+    cell that that has a 'TrackObjects_LinkType' of 1 that shares the same parent cell. This function gives of the first frame of all cells-through-time
+    and thus we can give them all a unique 'Cell ID' value.
+
+    This function returns a list of the starting cells found and a modified version of the input dataframe where the starting cells have been removed.
+    """
     cellID = 1
     cellList = []
     cellsToRemove = []
     count = 0
     
-    #get the cells that start lineages and splits, and start separate the cells
-    #these cells have a LinkType == 0 and 2 repectively
     lineageStarts = df.loc[(df['TrackObjects_LinkType'] == 0) | (df['TrackObjects_LinkType'] == 2)]
-    
     for index,row in lineageStarts.iterrows():
         cell = []
         cell.append([cellID,row])
         cellList.append(cell)
         cellsToRemove.append(index)
         cellID += 1
-    #get the cells that are the other half of the splits (linkType of 2), 
-    #these have a LinkType of 1 but are associated with the splits by being in the same frame AND haveing the same parent
-    #print(count) the amount of 0's and 2's were correctly found
     for cell in cellList:
         if cell[-1][1]['TrackObjects_LinkType'] == 2:
             splitFrame = cell[-1][1]['ImageNumber']
             splitParent = cell[-1][1]['TrackObjects_ParentObjectNumber']
             otherDaughter = df.loc[(df['ImageNumber']==splitFrame) &
                                        (df['TrackObjects_ParentObjectNumber']==splitParent) & 
-                                       (df['TrackObjects_LinkType'] == 1)]  #do i deal with linktypes of 3 (mitosis) or 4 (orphans/gaps)?
+                                       (df['TrackObjects_LinkType'] == 1)]
             #for some reason the length of this .loc is sometimes 0, i think its because cells are moving
-            
             for index,row in otherDaughter.iterrows():
                 cell = []
                 cell.append([cellID,row])
@@ -68,10 +81,17 @@ def findStartingCells(df):
                 cellsToRemove.append(index)
                 cellID += 1
     df = df.drop(cellsToRemove)
-    return cellID, cellList, cellsToRemove, df
+    
+    return cellList, df
 
 
 def findNextCell(df,currentCell):
+    """
+    Now that we have the starting frame for each cell-through-time we can follow that cell frame by frame and connect them by giving all instances
+    of that cell-through-time the same Cell ID number. This is done by recursively calling this function until the end of a track is reached.
+
+    This functions returns a a list of lists containing all instances of a single, cell-through-time.
+    """
     cellsToRemove = []
     cellID = currentCell[-1][0]
     currentFrame = currentCell[-1][1]['ImageNumber']
@@ -86,10 +106,16 @@ def findNextCell(df,currentCell):
             cellsToRemove.append(index)
         df = df.drop(cellsToRemove)
         findNextCell(df,currentCell)
+        
     return currentCell
 
 
 def formatData(cellList):
+    """
+    This function takes the list of lists of lists of all the cells-through-time and converts it into a dataframe.
+
+    This function returns the new dataframe containing all the cells-through-time with their unique 'Cell ID' as a new column
+    """
     data = []
     for cell in cellList:
         spots = []
@@ -99,10 +125,17 @@ def formatData(cellList):
         spots = pd.DataFrame(spots)
         data.append(spots)
     data = pd.concat(data)
+    
     return data
 
 
 def findParents(data):
+    """
+    This function for any given instance of a cell-through-time, finds the parent, or mother, of that instance and records this as a new
+    column in the dataframe. It also records the area of the the parent.
+
+    This function returns this updated version of the dataframe.
+    """
     parents = []
     parentsArea = []
     for index, row in data.iterrows():
@@ -120,95 +153,45 @@ def findParents(data):
         else:
             for index, value in parentArea.iteritems():
                 parentsArea.append(value)
-    return parents,parentsArea
-
-
-def relativeTiming(group):
-    birthTime = group['Time (hr)'].iloc[0]
-    group['Relative Time'] = group['Time (hr)'] - birthTime
-    return group
-                 
-
-def main():
-    ###############################################
-    ####### USER INPUT INFORMATION START HERE ###########
-    
-    
-    #directory = R"C:\Users\bisso\Desktop\H26FullField4\\"
-    directory = '/Users/johnmallon/Downloads/'
-    file = "FilterObjects.csv"
-    #set colors for graphs
-    qualitative_colors = seaborn.color_palette("Set3")
-    pixelConversion = 0.065  #Tupan's pixel conversion to um for the 100X objective
-    timeInterval = 15/60  #in hours
-    outputFileName = 'test.csv'
-    outputColumnOrder = ['Lineage','cell ID','Location_Center_X','Location_Center_Y','Shape',
-                         'ImageNumber','Time (hr)','Relative Time','Area','Aspect Ratio',
-                         'Circularity','Solidity','Parent','Parent Area']
-
-    ####### USER INPUT INFORMATION END HERE ###########
-    ##############################################
-    
-    
-    df = pd.read_csv(directory+file)
-    df = df.sort_values(by=['ImageNumber']) #sort all data by 'time'
-    df = calculateShape(df,pixelConversion)
-    df['Time (hr)'] = (df['ImageNumber']-1)*timeInterval
-    print(df.shape[0])
- 
-    
-    cellID,cellList,cellsToRemove,df = findStartingCells(df)
-    data = []
-    for cell in cellList:
-        stitchedCell = findNextCell(df,cell)
-        data.append(stitchedCell)
-    
-    
-    data = formatData(data)
-    print(data.shape[0])
-    
-    
-    cells = data.groupby('cell ID',group_keys=False)
-    data = cells.apply(relativeTiming)
-    
-    
-    #plot data with time and relative time
-    figure, ax = plt.subplots(dpi=300) 
-    seaborn.scatterplot(data=data,ax=ax,hue='cell ID',
-                   x='Time (hr)',y='AreaShape_Area_um',
-                   edgecolor="k",linewidth=0.75,palette=qualitative_colors,
-                   zorder=1.0)
-    ax.get_legend().set_visible(False)
- 
-    
-    parents,parentsArea = findParents(data)
     data['Parent'] = parents
     data['Parent Area'] = parentsArea
     
-    
-    
+    return data
 
 
-    #Calculate figure size in inches
-    width_pixels = 2048 #size of image when opened in FIJI
-    height_pixels = 2044 
-    dpi = 300
-    fig_size_inches = (width_pixels / dpi, height_pixels / dpi)
+def relativeTiming(group):
+    """
+    This function creates a new column in the dataframe that normalizes the time so that the first instance of all cells-through-time starts
+    at zero.
+
+    This function returns this modified version of the dataframe.
+    """
+    birthTime = group['Time (hr)'].iloc[0]
+    group['Relative Time'] = group['Time (hr)'] - birthTime
     
-    
-    
+    return group
+                 
+
+def visualizeTracking(data,outputDirectory):
+    """
+    This function takes the X,Y center points of instances of cells-through-time and draws lines between them and displays it on top of the
+    original segmentation '.tif' generated from Omnipose. This allows us to visually assess the quality of the tracking done by CellProfiler
+    since the second LAP tracking phase is never shown to you while CellProfiler runs.
+
+    This function saves a new '.tif' stack with the tracks visuallized and returns nothing.
+    """
     ###I think i have to group by cell ID####
     data['randomColor'] = data.groupby('TrackObjects_Label').apply(lambda x: np.random.rand(3,)).reset_index(level=0, drop=True)
     data['randomColor'].fillna('gray', inplace=True)
     cells = data.groupby('cell ID')
-
- 
-    
     im = io.imread('/Users/johnmallon/Downloads/131Frames.tif')
+    heightPixels, widthPixels, channels = image.shape
+    dpi = 300
+    figSizeInches = (width_pixels / dpi, height_pixels / dpi)
     stackedFrames = []
     plt.show()
     plt.ioff()
-    fig, tx = plt.subplots(dpi=dpi,figsize=fig_size_inches)
+    fig, tx = plt.subplots(dpi=dpi,figsize=figSizeInches)
     img = tx.imshow(im[0, :, :], cmap='gray')
     
     print('Tracking Cells...')
@@ -236,13 +219,63 @@ def main():
         plt.close()
     print('\nSAVING IMAGE...')
     trackingImage = np.stack(stackedFrames)
-    #io.imsave('/Users/johnmallon/Downloads/tracks.tif', trackingImage)
-    #select data to output
+    io.imsave(outputDirectory+'tracks.tif', trackingImage)
+    
+def main():
+    #####################################################
+    ####### USER INPUT INFORMATION START HERE ###########
+    inputDirectory = '/Users/johnmallon/Downloads/'
+    inputFileName = "FilterObjects.csv"
+    qualitative_colors = seaborn.color_palette("Set3")
+    pixelConversion = 0.065  # Pixel conversion to um for a 100X objective using Bisson Lab Microscope (Tupan)
+    timeInterval = 15/60  #in hours
+    outputDirectory = '/Users/johnmallon/Downloads/'
+    outputFileName = 'cellsThroughTime.csv'
+    outputColumnOrder = ['Lineage','cell ID','Location_Center_X','Location_Center_Y','Shape',
+                         'ImageNumber','Time (hr)','Relative Time','Area','Aspect Ratio',
+                         'Circularity','Solidity','Parent','Parent Area']
+    ####### USER INPUT INFORMATION END HERE #############
+    #####################################################
+    
+    df = pd.read_csv(inputDirectory+inputFileName)
+    df = df.sort_values(by=['ImageNumber']) #sort all data by 'time'
+    df = calculateShape(df,pixelConversion)
+    df['Time (hr)'] = (df['ImageNumber']-1)*timeInterval
+    initialDataSize = df.shape[0]
+    
+    cellList,df = findStartingCells(df)
+    data = []
+    for cell in cellList:
+        stitchedCell = findNextCell(df,cell)
+        data.append(stitchedCell)
+    data = formatData(data)
+    finalDataDize = data.shape[0]
+    try:
+        if initialDataSize != finalDataDize:
+            raise ValueError("Something went wrong with the concatenation of cells!")
+        else:
+            print('Cells successfully connected through time!')
+    except ValueError as e:
+        print(f"Error: {e}")
+    
+    cells = data.groupby('cell ID',group_keys=False)
+    data = cells.apply(relativeTiming)
+    data = findParents(data)
+    visualizeTracking(data,outputDirectory)
+
+     #plot data with time and relative time
+    figure, ax = plt.subplots(dpi=300) 
+    seaborn.scatterplot(data=data,ax=ax,hue='cell ID',
+                   x='Time (hr)',y='AreaShape_Area_um',
+                   edgecolor="k",linewidth=0.75,palette=qualitative_colors,
+                   zorder=1.0)
+    ax.get_legend().set_visible(False)
+    
     data = data.rename(columns={'TrackObjects_Label': 'Lineage'})
     data = data.rename(columns={'AreaShape_Area_um': 'Area'})
     data = data.rename(columns={'AreaShape_Solidity': 'Solidity'})
     outputDF = data[outputColumnOrder] 
-    #outputDF.to_csv(directory+outputFileName, index=False, header=True)     
-           
-        
+    outputDF.to_csv(outputDirectory+outputFileName, index=False, header=True)                
+
+
 main()
