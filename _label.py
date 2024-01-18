bc16b21a1b6cbd986019ecd617955ae4281f7265
Archaea-Lab <126028630+Archaea-Lab@users.noreply.github.com>
2024-01-18 12:09:55 -0500

    Update _label.py

--- _label.py
+++ _label.py
@@ -2,26 +2,22 @@
 """
 Created on Fri Dec  8 11:02:40 2023
 
-@author: bisso
+@author: johnmallon
 
 This program will open a folder of files and add the extension to the file name "_label"
 """
-
-
 import os
 
 
 def main():
-    #open folder
     folder = R"H:\Halofilin Shape Transitions\KO Shape Transitions\HalB KO\HalBDisktoRod_FullField\masks\\"
     files = os.listdir(folder)
     for file in files:
-       if os.path.isfile(os.path.join(folder, file)):
-           newName = file.split('.')[0] +'_label.tif'
-           #Rename the file
-           oldPath = os.path.join(folder, file)
-           newPath = os.path.join(folder, newName)
-           os.rename(oldPath, newPath)
+        oldPath = os.path.join(folder, file)
+        if os.path.isfile(oldPath):
+            newName = file.split('.')[0] +'_label.tif'
+            newPath = os.path.join(folder, newName)
+            os.rename(oldPath, newPath)
 
 main()
 
