28536f8fb816ffcffd78a3248b63358aa3627332
Archaea-Lab <126028630+Archaea-Lab@users.noreply.github.com>
2024-01-18 13:41:12 -0500

    Update outlinesToStack.py

--- outlinesToStack.py
+++ outlinesToStack.py
@@ -1,9 +1,17 @@
+"""
+author: johnmallon
+
+This script takes the individual segmentation images for each frame that omnipose outputs and stiched them together into a single stack and displays the stack. You should MANUALLY save the stack after inspecting it.
+
+This script should be run in the imageJ macro window.
+"""
 from ij import IJ, ImagePlus, ImageStack
 import os
 import re
 
 
 def main():
+	print("Starting...")
 	loadFolder = IJ.getDirectory("Input_directory")
 	files = os.listdir(loadFolder)
 	tempList = []
@@ -23,5 +31,7 @@ def main():
 	output_image = ImagePlus("Stacked Images", stack)
 	output_image.setDimensions(1,1,len(files)) #set channels, Z, and T appropriately
 	output_image.show()
+	print("Stack created")
+	print("Done!")
 
 main()
